fd8de0a1c7220a79859baedd34bdc2be8a019dfc
Yero-des <yero_zc@hotmail.com>
2023-12-05 15:09:30 -0500

    Lab 14

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'appAngular4';
+export class AppComponent implements OnInit {
+
+  angForm: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.angForm = this.fb.group({
+      name: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      birthdate: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      passwordRepeat: ['', [Validators.required, Validators.minLength(8), this.passwordValidator()]],
+    });
+  };
+
+  passwordValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
+      const password = this.angForm.get('password')?.value;
+      const repeat_password = control.value;
+
+      if (password !== repeat_password) {
+        return { mismatch: true };
+      }
+
+      return null;
+    };
+  }
+
+  onClickSubmit(formData : any) {
+    console.log(this.angForm.value);
+    alert("Your email is : " + formData.email);
+  }
+
+  ngOnInit(): void {
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
